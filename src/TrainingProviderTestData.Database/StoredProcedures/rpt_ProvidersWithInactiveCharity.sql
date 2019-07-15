10792403a4b322fcfc1a22aece0039fc9c2d2ea1
Dave Read <david.read@digital.education.gov.uk>
2019-07-15 10:48:34 +0100

    Add reports for organisations on UKRLP with inactive companies/charities

---
+++ src/TrainingProviderTestData.Database/StoredProcedures/rpt_ProvidersWithInactiveCharity.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[rpt_ProvidersWithInactiveCharity]
+AS
+	SELECT
+	ukrlp.[UKPRN],
+	ukrlp.[LegalName],
+	ukrlp.[TradingName],
+	ukrlp.[PrimaryVerificationSource],
+	ukrlp.[CompanyNumber],
+	ukrlp.[CharityNumber],
+	ukrlp.[Status],
+	ccdata.DateRegistered,
+	ccdata.DateRemoved
+	FROM [dbo].[UKRLPData] ukrlp
+	INNER JOIN CharityCommissionData ccdata
+	ON ccdata.CharityNumber = ukrlp.CharityNumber
+	WHERE ukrlp.charitynumber NOT IN ('')
+	AND ukrlp.[status]  = 'Active'
+	AND ccdata.DateRemoved IS NOT NULL
---
+++ src/TrainingProviderTestData.Database/StoredProcedures/rpt_ProvidersWithInactiveCompany.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[rpt_ProvidersWithInactiveCompany]
+AS
+	SELECT DISTINCT
+	ukrlp.[UKPRN],
+	ukrlp.[LegalName],
+	ukrlp.[TradingName],
+	ukrlp.[PrimaryVerificationSource],
+	ukrlp.[CompanyNumber],
+	ukrlp.[CharityNumber],
+	ukrlp.[Status] AS [UKRLP Status],
+	chdata.CompanyStatus AS [Companies House Status],
+	chdata.IncorporationDate,
+	chdata.DissolutionDate
+	FROM [dbo].[UKRLPData] ukrlp
+	INNER JOIN CompaniesHouseData chdata
+	ON chdata.CompanyNumber = ukrlp.CompanyNumber
+	WHERE ukrlp.CompanyNumber NOT IN ('')
+	AND ukrlp.[status]  = 'Active'
+	AND chdata.CompanyStatus NOT IN ('Active')
